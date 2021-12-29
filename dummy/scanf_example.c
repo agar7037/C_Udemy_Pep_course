d022bab76bd314712d8ad74b4fc2ceaf2a055007
Alex_Garrett_Jones <alexander.garrett.jones@gmail.com>
2021-12-29 15:45:49 +1100

    code blocks project

---
+++ dummy/scanf_example.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    char str[100]; //<--- define a character array stored inside variable str of size 100
+    int i; //<-- define a variable i which is an integer
+
+    printf("Enter a value: ");
+    scanf("%d %s", &i, str); //<--two arguments first arg "%s,%d" defines data type expected
+//"str, &i" defines the destination variables for the two expected data types
+//user will enter an array of characters of no more than 100, and then an integer - scanf stores char array in str variable
+//stores integer in variable i by using a pointer to varaible i.
+    printf("\nYou entered: %d\n", i); // print f retrieves values stored in variables str and i
+    return 0;
+}
