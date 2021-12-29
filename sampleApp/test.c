2890e087361b5f116a37faa88b2752e0d53f27b4
Alex_Garrett_Jones <alexander.garrett.jones@gmail.com>
2021-12-29 15:44:27 +1100

    sprintf example code with comments

--- sampleApp/test.c
+++ sampleApp/test.c
@@ -1,3 +1,8 @@
+/*
+Author: Alex Garrett-Jones
+Purpose: test program for learning how to configure vscode for C program building and debugging
+Date: 28/12/2021
+*/
 #include <stdio.h> 
 
 int main() {
