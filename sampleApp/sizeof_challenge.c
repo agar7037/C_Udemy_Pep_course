04d5be9fdcb567284fa09326187c5f71bdabed34
Alex_Garrett_Jones <alexander.garrett.jones@gmail.com>
2022-01-26 17:55:09 +1100

    added required include statements to sizeof_challenge

--- sampleApp/sizeof_challenge.c
+++ sampleApp/sizeof_challenge.c
@@ -3,7 +3,8 @@ Auther: Alex Garrett-Jones
 Date: 26/01/22 
 Purpose: challenge to print the byte size of common C data types 
 */
-
+#include <stdio.h>
+#include <stdlib.h>
 int main() 
 {
     printf("size of int: %zd\n", sizeof(int));
@@ -12,5 +13,8 @@ int main()
     printf("size of long long: %zd\n", sizeof(long long));
     printf("size of double: %zd\n", sizeof(double));
     printf("size of long doube: %zd\n",sizeof(long double));
+    printf("size of float: %zd\n", sizeof(float));
+    printf("size of short: %zd\n", sizeof(short));
+
     return 0; 
 }
