7e275ff0f9f1d3f55ac630169d832bcd7fc86691
Alex_Garrett_Jones <alexander.garrett.jones@gmail.com>
2022-02-09 18:02:50 +1100

    Written code for counting number of entries obtained from our config file

---
+++ sampleApp/delimit_count.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(void)
+{   const char* config = "russia_config.txt";
+    FILE *fp = fopen(config,"r");
+    int BUFFERSIZE = sizeof(wchar_t)*100; 
+    char s[BUFFERSIZE]; 
+    int entry_count = 0;
+// check that file exists 
+    if (fp == NULL){
+        printf("File does not exist");
+        return 0; 
+    }
+// cycle through file counting delimeter char '|'
+    while (fgets(s,BUFFERSIZE, fp) != NULL)
+    {
+        if (s[0] == '|')
+        {
+            entry_count++; 
+        }
+
+    }
+    
+    printf("number of entries: %d", entry_count);
+    fclose(fp);
+    return entry_count; 
+}
